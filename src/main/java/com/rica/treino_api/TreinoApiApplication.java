0bac4ae1a7840b441cc347a650f3ae9ceeacc47f
Ricardo Gomes <rcdgds@gmail.com>
2026-02-23 16:50:21 +0000

    second commit

---
+++ src/main/java/com/rica/treino_api/TreinoApiApplication.java
@@ -0,0 +1,13 @@
+package com.rica.treino_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rica/treino_api/TreinoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rica.treino_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
